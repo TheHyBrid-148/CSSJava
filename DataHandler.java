3bb802ddcb683ec8479a150f1b73afc7eace1580
missbiancss <mariabiancaasanion@gmail.com>
2025-03-09 21:41:36 +0800

    Update DataHandler.java

--- DataHandler.java
+++ DataHandler.java
@@ -1,20 +1,20 @@
 import java.io.*;
 
 public class DataHandler {
-    public void saveData(Subdivision subdivision, String filePath) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
-            oos.writeObject(subdivision); 
-            System.out.println("Data saved successfully to " + filePath);
+    public void saveData(Subdivision subdivision, String filename) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(subdivision);
+            System.out.println("Data saved successfully to " + filename);
         } catch (IOException e) {
             System.err.println("Error saving data: " + e.getMessage());
         }
     }
 
-    public Subdivision loadData(String filePath) {
+    public Subdivision loadData(String filename) {
         Subdivision subdivision = null;
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))) {
-            subdivision = (Subdivision) ois.readObject(); 
-            System.out.println("Data loaded successfully from " + filePath);
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            subdivision = (Subdivision) ois.readObject();
+            System.out.println("Data loaded successfully from " + filename);
         } catch (IOException | ClassNotFoundException e) {
             System.err.println("Error loading data: " + e.getMessage());
         }
