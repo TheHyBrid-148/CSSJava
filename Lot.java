000c0d3a57d667610f2e9c8f4998dc84f888c105
Joelvblgt <jvgbalagot@mymail.mapua.edu.ph>
2025-03-10 00:42:04 +0800

    Update Lot.java

--- Lot.java
+++ Lot.java
@@ -8,5 +8,51 @@
  * @author acer nitro v 15
  */
 public class Lot {
+    private int id;          
+    private double size;     
+    private double price;    
+    private String location; 
+    private String status;   
+
     
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
+
